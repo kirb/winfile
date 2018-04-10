6ea2e11784caadbc8560fed6bc0ea6e06de9060c
Adam Demasi <adam@hbang.com.au>
2018-04-10 16:50:22 +0930

    Add comctl 6.0 to linker flags to get visual styles on controls.

--- src/winfile.c
+++ src/winfile.c
@@ -7,6 +7,9 @@
 
 ********************************************************************/
 
+#pragma comment(linker, "\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
+	processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
 #define _GLOBALS
 #include "winfile.h"
 #include "lfn.h"
